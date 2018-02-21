25123553b11fa069ba3d2e3a7064908fa7c0721b
robbert van hove <robbertvanhove1@gmail.com>
2018-02-21 21:41:44 +0100

    Project phonegap gestart; Lay-out klaar

---
+++ Cordova/project/DSMTW/www/js/app.js
@@ -0,0 +1,18 @@
+$(function () {
+    document.addEventListener("deviceready", onDeviceReady, false);
+    $('.button-collapse').sideNav();
+
+    // klik op link in navigatie
+    $('.side-nav a').click(function () {
+        $('.spa').hide();
+        $('#' + $(this).data('show')).show();
+        $('.button-collapse').sideNav('hide');
+    });
+
+    
+});
+
+function onDeviceReady() {
+    console.log('Device is ready');
+    
+};
