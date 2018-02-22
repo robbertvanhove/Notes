4284cbd5a963ec59abf9329e04729a3eca816621
robbert van hove <robbertvanhove1@gmail.com>
2018-02-22 10:25:15 +0100

    WPF h3

---
+++ WPF/H1/OverzichtWerknemers/OverzichtWerknemers/Werknemer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace OverzichtWerknemers
+{
+    class Werknemer : INotifyPropertyChanged
+    {
+        private string naam;
+        private string voornaam;
+        private string afdeling;
+
+        public string Naam
+        {
+            get
+            {
+                return naam;
+            }
+
+            set
+            {
+                naam = value;
+            }
+        }
+
+        public string Voornaam
+        {
+            get
+            {
+                return voornaam;
+            }
+
+            set
+            {
+                voornaam = value;
+            }
+        }
+
+        public string Afdeling
+        {
+            get
+            {
+                return afdeling;
+            }
+
+            set
+            {
+                afdeling = value;
+            }
+        }
+
+        public Werknemer(string naam, string voornaam, string afdeling)
+        {
+            Naam = naam;
+            Voornaam = voornaam;
+            Afdeling = afdeling;
+        }
+
+        public override string ToString()
+        {
+            return naam + ' ' + voornaam;
+        }
+
+        // Declareren event
+        public event PropertyChangedEventHandler PropertyChanged;
+        // OnPropertyChanged zorgt voor update property value in alle bindings
+        private void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
---
+++ WPF/H1/OverzichtWerknemers/OverzichtWerknemers/Werknemers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+
+namespace OverzichtWerknemers
+{
+    class Werknemers : ObservableCollection<Werknemer>
+    {
+        public  Werknemers()
+        {
+            Add(new Werknemer("Peeters","Jef", "Marketing"));
+            Add(new Werknemer("Verboven", "Anna", "Sales"));
+            Add(new Werknemer("De Winter", "Karen", "Marketing"));
+            Add(new Werknemer("Dierckx", "Daisy", "R&D"));
+            Add(new Werknemer("Aerts", "Gert", "Sales"));
+            Add(new Werknemer("Horemans", "Sam", "ICT"));
+            Add(new Werknemer("Somers", "Dirk", "Marketing"));
+            Add(new Werknemer("Geerts", "Tuur", "R&D"));
+            Add(new Werknemer("Vervloesem", "An", "Sales"));
+            Add(new Werknemer("Berx", "Karl", "ICT"));
+        }
+    }
+}
